52d57461077a13bc2bc8153ddc71f782400bcaa7
Fernanda Palacios <fernandapalacios.dev@gmail.com>
2021-04-23 17:24:50 -0500

    [UPDATE] Added dsl for build app node with jenkins

---
+++ dsl/nodedsl.groovy
@@ -0,0 +1,37 @@
+job('Aplicacion Node.js DSL') {
+    description('Aplicación Node JS DSL de Jenkins')
+    scm {
+        git('https://github.com/EveFer/node-jenkins.git', 'main') { node ->
+            node / gitConfigName('EveFer')
+            node / gitConfigEmail('fernandapalacios.dev@gmail.com')
+        }
+    }
+    triggers {
+        scm('H/7 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs')
+    }
+    steps {
+        shell("npm install")
+    }
+    publishers {
+	slackNotifier {
+            notifyAborted(true)
+            notifyEveryFailure(true)
+            notifyNotBuilt(false)
+            notifyUnstable(false)
+            notifyBackToNormal(true)
+            notifySuccess(true)
+            notifyRepeatedFailure(false)
+            startNotification(false)
+            includeTestSummary(false)
+            includeCustomMessage(false)
+            customMessage(null)
+            sendAs(null)
+            commitInfoChoice('NONE')
+            teamDomain(null)
+            authToken(null)
+        }
+    }
+}
